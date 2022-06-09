0bdafc9ab75d032c80fbbee4c6571117d6d6b1f7
naveench909 <naveen.kumar@roro.io>
2022-06-09 11:05:25 +0530

    changed appspec.yml file

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl --silent -fsSL https://deb.nodesource.com/setup_16.x | bash -
+curl --silent -fsSL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y nodejs
 
 npm install -g pm2
